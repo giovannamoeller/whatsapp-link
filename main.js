7be73a3b4cf478daa861508ff1c4a9d740d040db
Giovanna <giovannagmoeller@gmail.com>
2022-07-29 01:34:45 -0300

    unmasking values

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const input = document.querySelector('input')
 const maskOptions = {
     mask: '(00) 00000-0000'
 }
-let cellphoneNumber
-let url
 
 button.addEventListener('click', function () {
-  cellphoneNumber = input.value
-  url = `https://api.whatsapp.com/send?phone=55${cellphoneNumber}`
+  const cellphoneNumber = input.value
+  const formattedCellphoneNumber = cellphoneNumber.replace('(', '').replace(')', '').replace(' ', '').replace('-', '')
+  const url = `https://api.whatsapp.com/send?phone=55${formattedCellphoneNumber}`
   window.open(url, '_blank').focus()
 })
 
