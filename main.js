bf6817a49110fe8f08f7561796c3cd7346230785
Lucas <lucasjapa10@hotmail.com>
2022-07-29 14:03:27 -0300

    input vaziore retorna

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const button = document.querySelector('button')
+const button = document.querySelector('#button')
 const input = document.querySelector('input')
 const maskOptions = {
     mask: '(00) 00000-0000'
 }
 
-button.addEventListener('click', function () {
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+  let errorText = document.querySelector('#error');
+  if(input.value == '') {
+    errorText.innerHTML = 'Digite um número!';
+    return
+  }else {
+    errorText.innerHTML = '';
+  }
   const cellphoneNumber = input.value
   const formattedCellphoneNumber = cellphoneNumber.replace('(', '').replace(')', '').replace(' ', '').replace('-', '')
   const url = `https://api.whatsapp.com/send?phone=55${formattedCellphoneNumber}`
